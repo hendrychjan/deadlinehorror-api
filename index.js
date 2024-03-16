f7e99c56589871fa5b5f5a8f2108d43a0de9a7d9
hendrychjan <jan.honza.hendrych@gmail.com>
2024-03-16 21:47:01 +0100

    Change default port to 3010

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { config as routerConfig } from "./src/startup/router.js";
 import { connect as dbConnect } from "./src/startup/db.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 // Load startup configs
 envConfig();
